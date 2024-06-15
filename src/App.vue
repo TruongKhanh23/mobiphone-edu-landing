55a97b098d54e7c8e693720473017ac5603b4f23
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2024-06-15 10:19:46 +0700

    MBF-002: Update welcome page, update favicon, title

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
     <!-- <Navigator /> -->
-    <div class="max-w-3xl mx-auto my-6">
-      <p class="text-center">Welcome to Mobiphone Edu Landing Page</p>
+    <div
+      class="max-w-3xl mx-auto my-6 flex flex-col items-center justify-center text-center"
+    >
+      <div>
+        <img :src="logoMobiEdu" class="w-36 mb-4" alt="Mobi Edu Logo" />
+      </div>
+      <div>
+        <p>Welcome to Mobiphone Edu Landing Page</p>
+      </div>
       <!-- <router-view /> -->
     </div>
   </div>
@@ -10,12 +17,16 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import logoMobiEdu from "../../assets/logo-mobi-edu.png";
 
-import Navigator from "./components/shared/Navigator.vue";
+//import Navigator from "./components/shared/Navigator.vue";
 
 export default defineComponent({
-  components: {
-    Navigator,
+  // components: {
+  //   Navigator,
+  // },
+  setup() {
+    return { logoMobiEdu };
   },
 });
 </script>
