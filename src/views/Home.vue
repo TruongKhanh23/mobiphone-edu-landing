57fa0d88619d2005e0a75acf58c96ebca49ad495
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2024-06-17 21:14:12 +0700

    MBP-008: Make LeftRightBarIcon.vue as a component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,28 +43,8 @@
       </div>
     </div>
 
-    <!-- Floating icons -->
-    <div
-      class="absolute top-1/2 right-12 transform -translate-y-1/2 flex flex-col gap-2"
-    >
-      <div
-        v-for="(icon, index) in icons"
-        :key="index"
-        class="cursor-pointer transition-transform transform hover:scale-110 z-2"
-        @click="showPopup(index)"
-      >
-        <img :src="icon" alt="Icon" class="w-10 h-10" />
-      </div>
-    </div>
-
-    <!-- Popup -->
-    <div
-      v-if="isPopupVisible"
-      class="absolute top-1/2 left-12 w-1/5 max-h-4/5 overflow-y-auto bg-white bg-opacity-90 p-5 shadow-lg transform -translate-y-1/2 z-50 rounded-lg"
-      :style="{ height: popupHeight }"
-    >
-      <p>{{ randomText }}</p>
-    </div>
+    <!-- Left Right Bar Icon -->
+    <LeftRightBarIcon />
   </div>
 </template>
 
@@ -74,12 +54,8 @@ import LoadingModal from "@/components/LoadingModal.vue";
 import SchoolSecondFloor from "@/views/School/SchoolSecondFloor.vue";
 import { handlePopup, open, close } from "@/composables/loadingModal/index.js";
 import "@/assets/css/home.css";
-import consultantIcon from "@/assets/icon/consultant-icon.png";
-import contributeIcon from "@/assets/icon/contribute-icon.png";
-import networkIcon from "@/assets/icon/network-icon.png";
-import personIcon from "@/assets/icon/person-icon.png";
-import settingIcon from "@/assets/icon/setting-icon.png";
 import VideoBackground from "@/views/Home/VideoBackground.vue";
+import LeftRightBarIcon from "@/views/Home/LeftRightBarIcon.vue";
 
 export default {
   name: "App",
@@ -87,23 +63,12 @@ export default {
     LoadingModal,
     SchoolSecondFloor,
     VideoBackground,
+    LeftRightBarIcon,
   },
   setup() {
     const { isOpenLoadingModal } = handlePopup();
     const isOpenSecondFloor = ref(false);
     const isOpenSchool = ref<boolean>(false);
-    const isPopupVisible = ref(false);
-    const randomText = ref("");
-    const popupHeight = ref("auto");
-    const activeIconIndex = ref<number | null>(null);
-
-    const icons = [
-      consultantIcon,
-      contributeIcon,
-      networkIcon,
-      personIcon,
-      settingIcon,
-    ];
 
     function onClickButton() {
       isOpenSecondFloor.value = open();
@@ -121,25 +86,6 @@ export default {
       isOpenSchool.value = false;
     };
 
-    const showPopup = (index: number) => {
-      if (activeIconIndex.value === index) {
-        // If the same icon is clicked again, toggle the popup visibility
-        isPopupVisible.value = !isPopupVisible.value;
-      } else {
-        const texts = [
-          "Random text 1",
-          "Random text 2",
-          "Random text 3",
-          "Random text 4",
-          "Random text 5",
-        ];
-        randomText.value = texts[index];
-        popupHeight.value = "auto"; // Adjust based on text length
-        isPopupVisible.value = true;
-        activeIconIndex.value = index;
-      }
-    };
-
     return {
       isOpenLoadingModal,
       isOpenSecondFloor,
@@ -148,17 +94,11 @@ export default {
       handleCloseSecondFloor,
       handleCloseSchoolSecondFloor,
       openSchoolSecondFloor,
-      icons,
-      isPopupVisible,
-      randomText,
-      popupHeight,
-      activeIconIndex,
-      showPopup,
     };
   },
 };
 </script>
 
 <style scoped>
-/* Floating icons styles */
+/* Scoped styles specific to App */
 </style>
---
+++ src/views/Home/LeftRightBarIcon.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <!-- Floating icons -->
+    <div
+      class="absolute top-1/2 right-12 transform -translate-y-1/2 flex flex-col gap-2"
+    >
+      <div
+        v-for="(icon, index) in icons"
+        :key="index"
+        class="cursor-pointer transition-transform transform hover:scale-110 z-2"
+        @click="showPopup(index)"
+      >
+        <img :src="icon" alt="Icon" class="w-10 h-10" />
+      </div>
+    </div>
+
+    <!-- Popup -->
+    <div
+      v-if="isPopupVisible"
+      class="absolute top-1/2 left-12 w-1/5 max-h-4/5 overflow-y-auto bg-white bg-opacity-90 p-5 shadow-lg transform -translate-y-1/2 z-50 rounded-lg"
+      :style="{ height: popupHeight }"
+    >
+      <p>{{ randomText }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+import consultantIcon from "@/assets/icon/consultant-icon.png";
+import contributeIcon from "@/assets/icon/contribute-icon.png";
+import networkIcon from "@/assets/icon/network-icon.png";
+import personIcon from "@/assets/icon/person-icon.png";
+import settingIcon from "@/assets/icon/setting-icon.png";
+
+export default {
+  name: "LeftRightBarIcon",
+  setup() {
+    const isPopupVisible = ref(false);
+    const randomText = ref("");
+    const popupHeight = ref("auto");
+    const activeIconIndex = ref<number | null>(null);
+
+    const icons = [
+      consultantIcon,
+      contributeIcon,
+      networkIcon,
+      personIcon,
+      settingIcon,
+    ];
+
+    const texts = [
+      "Random text 1",
+      "Random text 2",
+      "Random text 3",
+      "Random text 4",
+      "Random text 5",
+    ];
+
+    const showPopup = (index: number) => {
+      if (activeIconIndex.value === index) {
+        // If the same icon is clicked again, toggle the popup visibility
+        isPopupVisible.value = !isPopupVisible.value;
+      } else {
+        randomText.value = texts[index];
+        popupHeight.value = "auto"; // Adjust based on text length
+        isPopupVisible.value = true;
+        activeIconIndex.value = index;
+      }
+    };
+
+    return {
+      icons,
+      isPopupVisible,
+      randomText,
+      popupHeight,
+      activeIconIndex,
+      showPopup,
+    };
+  },
+};
+</script>
+
+<style scoped>
+/* Floating icons styles */
+</style>
