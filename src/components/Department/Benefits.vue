9210f0488a4317897f5d489a2d553581abc38692
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2024-06-20 23:08:05 +0700

    MBP-014: Department Benefits

--- src/components/Department/Benefits.vue
+++ src/components/Department/Benefits.vue
@@ -9,8 +9,8 @@
         :class="`${benefit.parentClass} hover:scale-110 border-2 border-white bg-white text-black shadow-effect flex rounded-md items-center justify-center`"
       >
         <!--Column 1 30%-->
-        <div :class="`${benefit.numberClass} w-3/10`">
-          {{ benefit.number }}
+        <div class="w-3/10">
+          <img :src="benefit.icon" :class="benefit.iconClass" />
         </div>
         <!-- Column 2 70%-->
         <div :class="`${benefit.contentClass} w-7/10 ml-6`">
@@ -28,6 +28,9 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import magnifyingGlass from "@/assets/icon/magnifying-glass.png";
+import statistic from "@/assets/icon/statistic.png";
+import multipleEclipse from "@/assets/icon/multiple-eclipse.png";
 
 export default defineComponent({
   name: "Benefits",
@@ -35,29 +38,29 @@ export default defineComponent({
     // Đoạn text của các lợi ích
     const benefits = [
       {
-        number: "01",
+        icon: statistic,
         title: "Đánh giá hiệu quả giáo dục thông",
         description: "qua báo cáo & thống kê.",
-        numberClass: "text-left font-black text-4xl",
+        iconClass: "w-10 h-10",
         contentClass: "w-[75%] float-right",
         titleClass: "font-bold text-center italic",
         descriptionClass: "text-center text-sm",
         parentClass: "w-[35vw] h-[13vh]",
       },
       {
-        number: "02",
+        icon: magnifyingGlass,
         title: "Đánh giá hiệu quả giáo dục thông",
         description: "qua báo cáo & thống kê.",
-        numberClass: "text-left font-black text-4xl",
+        iconClass: "w-10 h-10",
         contentClass: "w-[75%] float-right",
         titleClass: "font-bold text-center italic",
         descriptionClass: "text-center text-sm",
         parentClass: "w-[35vw] h-[13vh]",
       },
       {
-        number: "03",
+        icon: multipleEclipse,
         title: "Hỗ trợ quản lý hệ thống giáo dục.",
-        numberClass: "text-left font-black text-4xl",
+        iconClass: "w-10 h-10",
         contentClass: "w-[75%] float-right",
         titleClass: "font-bold text-center italic",
         descriptionClass: "text-center text-sm",
