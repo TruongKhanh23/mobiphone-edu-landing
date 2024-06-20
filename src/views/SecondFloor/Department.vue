f54dd382a37b0ce8348f525d0c9b9c0e93e1a141
TruongKhanh23 <truongnguyenkhanh230800@gmail.com>
2024-06-20 20:28:16 +0700

    MBP-014: Department (WIP)

--- src/views/SecondFloor/Department.vue
+++ src/views/SecondFloor/Department.vue
@@ -6,14 +6,44 @@
       :handleCancel="handleCloseDepartmentFeatureOne"
       class="z-20"
     />
-    <div @click="handleClick" class="image-container">
+    <div @click="handleClick" class="image-container overflow-hidden">
       <img
         :src="backgroundDepartmentFeatures"
         alt="backgroundDepartmentFeatures"
       />
-      <div class="overlay-text">Text for Features</div>
+      <div
+        class="feature-title font-montserratExtraBold font-black text-4xl text-[#D7DEFF]"
+      >
+        TÍNH NĂNG
+      </div>
+
+      <div class="feature-one">
+        <div class="w-[23vw] h-40 hover:scale-110">
+          <div class="text-right font-black text-4xl text-[#EEBE3A]">01</div>
+          <div class="w-[75%]">
+            <div class="font-bold text-justify text-[#0C9DEE] italic">
+              ĐỊNH DANH cây học liệu theo chương học -
+            </div>
+            <div class="text-right text-sm">
+              bài giảng chi tiết cho toàn Sở/Phòng.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="feature-two">
+        <div class="border-4 border-white w-[30vw] h-40 bg-[red]"></div>
+      </div>
+      <div class="feature-three">
+        <div class="border-4 border-white w-[30vw] h-40 bg-[red]"></div>
+      </div>
+      <div class="feature-four">
+        <div class="border-4 border-white w-[25vw] h-40 bg-[red]"></div>
+      </div>
+      <div class="feature-five">
+        <div class="border-4 border-white w-[25vw] h-40 bg-[red]"></div>
+      </div>
     </div>
-    <div @click="handleClick" class="image-container">
+    <div @click="handleClick" class="image-container mt-[-10px]">
       <img
         :src="backgroundDepartmentBenefits"
         alt="backgroundDepartmentBenefits"
@@ -104,6 +134,64 @@ export default {
   font-size: 20px;
   text-align: center;
   background-color: rgba(0, 0, 0, 0.5); /* Nền đen mờ để chữ dễ đọc hơn */
+  padding: 10px;
+  border-radius: 5px;
+}
+.feature-title {
+  position: absolute;
+  top: 12%;
+  left: 16%;
+  transform: translate(-50%, -50%);
+  padding: 10px;
+  border-radius: 5px;
+}
+.feature-one {
+  position: absolute;
+  top: 40%;
+  left: 16%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 5px;
+}
+.feature-two {
+  position: absolute;
+  top: 85%;
+  left: 20%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 5px;
+}
+.feature-three {
+  position: absolute;
+  top: 83%;
+  left: 78%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 5px;
+}
+.feature-four {
+  position: absolute;
+  top: 53%;
+  left: 86%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 5px;
+}
+.feature-five {
+  position: absolute;
+  top: 23%;
+  left: 86%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 20px;
   padding: 10px;
   border-radius: 5px;
 }
